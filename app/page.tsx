b6dadd26f15c6dc0f2ba0fa90b90afad42e83598
arvindeva <arvindeva@gmail.com>
2024-03-22 01:15:42 +0100

    fetch data from db successful

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import { db } from '@/db'
+import { users as usersTable } from '@/db/schema/users'
+
+export default async function Home() {
+  const users = await db.select().from(usersTable)
+
+  console.log(users)
   return (
     <main>
       <h1>Sortr</h1>
