2c4d10217976df3d1c8a979414151e29452e5a40
arvindeva <arvindeva@gmail.com>
2024-03-22 16:24:24 +0100

    fetch sortr by userid working on /me page. TODO: refactor everything please

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -3,6 +3,8 @@ import { Button } from '@/components/ui/button'
 import SignoutButton from '@/components/ui/sign-out-button'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+import { db, eq } from '@/db'
+import { sortrs as sortrsTable } from '@/db/schema/sortrs'
 
 export default async function ProfilePage() {
   const session = await auth()
@@ -11,12 +13,27 @@ export default async function ProfilePage() {
     redirect('/api/auth/signin?callbackUrl=/me')
   }
 
+  const result = await db
+    .select({
+      id: sortrsTable.id,
+      title: sortrsTable.title,
+    })
+    .from(sortrsTable)
+    .where(eq(sortrsTable.userId, `${session.user.id}`))
+  console.log(result)
+
   return (
     <main className="space-y-8 max-w-screen-xl mx-auto">
       <h1 className="text-4xl">{session.user.name}</h1>
       <div>
         <h2 className="text-2xl">Your posts</h2>
-        <p>Looks like you don&apos;t have any sortrs...</p>
+        {result && (
+          <div>
+            {result.map((sortr) => {
+              return <div key={sortr.id}>{sortr.title}</div>
+            })}
+          </div>
+        )}
         <Link href="/create">
           <Button>Create a sortr</Button>
         </Link>
