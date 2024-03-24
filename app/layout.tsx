0a22237f59352af9e8cc1a6da80ec607a26a9278
Arvindeva Wibisono <arvindeva@gmail.com>
2024-03-24 12:57:18 +0100

    add top loader for links

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/app/globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
-import { ModeToggle } from '@/components/ui/mode-toggle'
+import NextTopLoader from 'nextjs-toploader'
 import Navbar from '@/components/ui/navbar'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
+          <NextTopLoader color="#ff0000" showSpinner={false} />
           {children}
         </ThemeProvider>
       </body>
