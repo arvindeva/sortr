54a90d756aad581a1adb3117c115216bd065b8a7
arvindeva <arvindeva@gmail.com>
2025-09-30 22:41:19 +0700

    remove apology banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { Analytics } from "@vercel/analytics/next";
 import { Providers } from "@/components/providers";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
-import { DataMigrationBanner } from "@/components/data-migration-banner";
 import NextTopLoader from "nextjs-toploader";
 
 import "./globals.css";
@@ -58,7 +57,6 @@ export default function RootLayout({
       >
         <Providers>
           <NextTopLoader color="#ff6b8a" showSpinner={false} height={3} />
-          <DataMigrationBanner />
           <Navbar />
           <div className="flex-1">{children}</div>
           <Footer />
